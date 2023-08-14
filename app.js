fcbd9bae4f2dcd020c0e38d1b155aebd9068eee8
Shawm93 <leeboon1993@gmail.com>
2023-08-15 01:07:23 +0800

    Modify JS and HTML file

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const submit = document.querySelector("button");
+const addForm = document.querySelector(".addForm");
+const addBook = document.querySelector(".addBook");
+const container = document.querySelector(".container");
+const input_form = document.querySelector(".input_form");
+const cancel = document.querySelector(".cancel");
 const title = document.querySelector("#title");
 const author = document.querySelector("#author");
 const pages = document.querySelector("#pages");
@@ -23,8 +27,20 @@ Book.prototype.addBookToLibrary = function() {
     myLibrary.push(this);
 }
 
-submit.addEventListener('click', () => {
-    console.log("submit");
+addForm.addEventListener('click', () => {
+    console.log('click');
+    container.classList.add("display-hidden");
+    input_form.classList.remove("display-hidden");
+})
+
+cancel.addEventListener('click', () => {
+    container.classList.remove("display-hidden");
+    input_form.classList.add("display-hidden");
+})
+
+addBook.addEventListener('click', () => {
+    container.classList.remove("display-hidden");
+    input_form.classList.add("display-hidden");
     let del_btn = document.createElement('button');
     let read_btn = document.createElement('button');
     del_btn.textContent = "Delete";
@@ -65,6 +81,7 @@ submit.addEventListener('click', () => {
 
     console.log(del_btns);
     del_btns.forEach(del_btn => del_btn.addEventListener('click', () => {
+        console.log('delete');
         Array.from(card).splice(Array.from(del_btns).indexOf(del_btn), Array.from(del_btns).indexOf(del_btn));
         read_btns = document.querySelectorAll(".read_btn");
         del_btns = document.querySelectorAll(".del_btn");
